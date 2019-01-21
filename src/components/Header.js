2834ce544f32fd8893d7f2488030d7a37fbae98f
chekwas88 <princechekwas@gmail.com>
2019-01-21 15:13:44 +0100

    make page responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,12 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
 
-const Header = (props) => {
-  const { randomImage } = props;
-  console.log(randomImage);
-  return (
-    <header>
-      <img src="https://images.vexels.com/media/users/3/143350/isolated/preview/150164edc7f28a716bfceae9dd58cf2c-coolface-trollface-meme-by-vexels.png" alt="trollface" />
-    </header>
-  );
-};
+const Header = () => (
+  <header>
+    <img src="https://images.vexels.com/media/users/3/143350/isolated/preview/150164edc7f28a716bfceae9dd58cf2c-coolface-trollface-meme-by-vexels.png" alt="trollface" />
+    <p>Meme Generator</p>
+  </header>
+);
 
 
 export default Header;
